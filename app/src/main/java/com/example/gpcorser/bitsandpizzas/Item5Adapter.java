96e0fbcd7f5c0a7c8ec6dfa0d0d63e1f65b0612f
Niklas <nmander@outlook.com>
2016-12-06 18:54:27 -0500

    Updated Faculty Page

--- app/src/main/java/com/example/gpcorser/bitsandpizzas/Item5Adapter.java
+++ app/src/main/java/com/example/gpcorser/bitsandpizzas/Item5Adapter.java
@@ -12,13 +12,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Item5Adapter extends ArrayAdapter<String>
 {
     private final Activity context;
     private final String[] itemName;
     private final Integer[] imgID;
+    private final String[] itemDesc;
+    private final String[] itemContact;
 
-    public Item5Adapter(Activity context, String[] itemName, Integer[] imgID)
+    public Item5Adapter(Activity context, String[] itemName, Integer[] imgID, String[] itemDesc,
+                        String[] itemContact)
     {
         super(context, R.layout.item5list, itemName);
         // TODO Auto-generated constructor stub
@@ -26,6 +31,8 @@ public class Item5Adapter extends ArrayAdapter<String>
         this.context=context;
         this.itemName=itemName;
         this.imgID=imgID;
+        this.itemDesc=itemDesc;
+        this.itemContact=itemContact;
     }
 
     public View getView(int position,View view,ViewGroup parent)
@@ -36,10 +43,12 @@ public class Item5Adapter extends ArrayAdapter<String>
         TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.image);
         TextView extratxt = (TextView) rowView.findViewById(R.id.textview1);
+        TextView extratxt2 = (TextView) rowView.findViewById(R.id.textview2);
 
         txtTitle.setText(itemName[position]);
         imageView.setImageResource(imgID[position]);
-        extratxt.setText("Description "+itemName[position]);
+        extratxt.setText(itemDesc[position]);
+        extratxt2.setText(itemContact[position]);
         return rowView;
     }
 
--- app/src/main/java/com/example/gpcorser/bitsandpizzas/Item5Fragment.java
+++ app/src/main/java/com/example/gpcorser/bitsandpizzas/Item5Fragment.java
@@ -54,6 +54,46 @@ public class Item5Fragment extends Fragment
             R.drawable.nophoto,
     };
 
+    String[] itemDesc ={
+            "Department Chair",
+            "Dean of the College of Science",
+            "Associate Dean of the College of Science",
+            "Associate Professor of CSIS",
+            "Assistant Professor of CSIS",
+            "Lecturer of CSIS",
+            "Assistant Professor of CSIS",
+            "Professor of CSIS",
+            "Assistant Professor of CSIS",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty",
+            "Adjunct Faculty"
+    };
+
+    String[] itemContact ={
+            "Science East 178  |  989-964-2044",
+            "Pioneer Hall 236B |  989-964-4144",
+            "Pioneer Hall 236  |  989-964-4144",
+            "Science East 175  |  989-964-4198",
+            "Science East 179  |  989-964-2756",
+            "Science East 173  |  989-964-4483",
+            "Science East 172  |  989-964-4191",
+            "Science East 176  |  989-964-4896",
+            "Science East 177  |  989-964-2528",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050",
+            "989-964-4050"
+    };
+
     public Item5Fragment() {
         // Required empty public constructor
     }
@@ -64,7 +104,7 @@ public class Item5Fragment extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_item5, container, false);
 
-        Item5Adapter adapter = new Item5Adapter(getActivity(), itemName, imgID);
+        Item5Adapter adapter = new Item5Adapter(getActivity(), itemName, imgID, itemDesc, itemContact);
         list = (ListView) view.findViewById(R.id.list);
         list.setAdapter(adapter);
 
